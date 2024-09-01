09fddfe2943cbd00ab1919d17de71871b4a3ffd3
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-01 22:52:32 +0530

    understnding code file from #include, namespace std

--- test/hello.cpp
+++ test/hello.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 
 //making custom namespace
-namespace mymethods{
-    void display(){
-        //comment
-    }
+// namespace mymethods{
+//     void display(){
+//         //comment
+//     }
+// }
+
+string myName(){
+    return "sahil is awesome";
 }
 
 int main(){
     cout << "Hello, C++ from sahil sharma" << endl;
 
-    //using my namespace
-    mymethods::display();
 
+    // std::cout<< "using namespace std;" << std::endl;
+    //using my namespace
+    // mymethods::display();
+    // string username = myName();
+    cout << myName() << endl;
     return 0;
 }

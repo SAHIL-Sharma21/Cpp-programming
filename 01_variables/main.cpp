b5ae15a6a773625d1141316d1168bca684f4d3c3
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-08 13:51:07 +0530

    variable and const keyword

---
+++ 01_variables/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    cout << "varibales and constant" << endl;
+
+    //varaibles
+    int score; //declare variable
+    score = 120; //initialized
+    int balance = 123;
+    balance = 799;
+
+    //const keyword
+    const int uid = 723232;
+
+    cout << "balance is:" << balance << endl;
+    cout << "uid of user is: " << uid << endl;
+    return 0;
+}
